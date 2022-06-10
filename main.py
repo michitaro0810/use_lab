bef2e8d574169c6f83915769792112974134ffaa
sato064 <satoberu@gmail.com>
2022-06-10 11:11:28 +0900

    fix main

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-# ボットトークンと署名シークレットを使ってアプリを初期化します
+# ボットトークンとソケットモードハンドラーを使ってアプリを初期化します
 app = App(
     token=os.environ.get("SLACK_BOT_TOKEN"),
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
 )
 
+# 'hello' を含むメッセージをリッスンします
+# 指定可能なリスナーのメソッド引数の一覧は以下のモジュールドキュメントを参考にしてください：
+# https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
 @app.message("hello")
 def message_hello(message, say):
     # イベントがトリガーされたチャンネルへ say() でメッセージを送信します
@@ -15,4 +18,4 @@ def message_hello(message, say):
 
 # アプリを起動します
 if __name__ == "__main__":
-    app.start(port=int(os.environ.get("PORT", 3000)))
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
